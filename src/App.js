93959017fb2592465f024777b65531a94f99a7e0
ClifAIsla <clifa.isla@gmail.com>
2022-03-17 16:34:06 -0500

    todos los botones operativos

--- src/App.js
+++ src/App.js
@@ -15,9 +15,26 @@ class App extends React.Component{
   }
       
   aumentarEdad1 = () => {
-    console.log("HOLA DESDE JS")
     this.setState({
       age1: this.state.age1 + 1
+    })
+  }
+
+  aumentarEdad2 = () => {
+    this.setState({
+      age2: this.state.age2 + 1
+    })
+  }
+
+  aumentarEdad3 = () => {
+    this.setState({
+      age3: this.state.age3 + 1
+    })
+  }
+
+  aumentarEdad4 = () => {
+    this.setState({
+      age4: this.state.age4 + 1
     })
   }
 
@@ -31,11 +48,17 @@ class App extends React.Component{
           <Boton firstName="Jane" lastName="Doe" happyBirtdhay={this.aumentarEdad1} />
 
 
-          <PersonCard firstName="John" lastName="Smith" age='88' haircolor="brown"/>
+          <PersonCard firstName="John" lastName="Smith" age={this.state.age2} haircolor="brown"/>
 
-          <PersonCard firstName="Millard" lastName="Filmore" age='50' haircolor="brown"/>
+          <Boton firstName="John" lastName="Smith" happyBirtdhay={this.aumentarEdad2} />
 
-          <PersonCard firstName="Maria" lastName="Smith" age='62' haircolor="brown"/>
+          <PersonCard firstName="Millard" lastName="Filmore" age={this.state.age3} haircolor="brown"/>
+
+          <Boton firstName="Millard" lastName="Filmore" happyBirtdhay={this.aumentarEdad3} />
+
+          <PersonCard firstName="Maria" lastName="Smith" age={this.state.age4} haircolor="brown"/>
+
+          <Boton firstName="Maria" lastName="Smith" happyBirtdhay={this.aumentarEdad4} />
       
       </div>
     )
